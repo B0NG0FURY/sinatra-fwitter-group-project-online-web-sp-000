3529d1551a43384eab0921c18a25f45fc602fbbd
bongofury <jtroutmanman@gmail.com>
2021-02-05 02:05:01 -0600

    added get routes for /tweets and /tweets/:id in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        @user = current_user
+        if @user
+            @tweets = Tweet.all
+            erb :"/tweets/index"
+        end
+    end
+
+    get '/tweets/:id' do
+        @user = current_user
+        if @user
+            @tweet = Tweet.find(params[:id])
+            erb :"/tweets/show"
+        end
+    end
 
 end
