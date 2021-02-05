8d6e694182482bce7fe37941561feb00a2923b6a
bongofury <jtroutmanman@gmail.com>
2021-02-05 01:57:27 -0600

    added 3 get routes in user controller: /user, /user/new, and /user/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
+    get '/user' do
+        @user = current_user
+        if @user
+            erb :"/users/index"
+        end
+    end
+
+    get '/user/new' do
+        erb :"/users/new"
+    end
+
+    get '/user/login' do
+        erb :"/users/login"
+    end
 
 end
