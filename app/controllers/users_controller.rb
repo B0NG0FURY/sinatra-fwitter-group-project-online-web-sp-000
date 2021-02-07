c5ee51d5bfccacc44cb787d95d753738618518c0
bongofury <jtroutmanman@gmail.com>
2021-02-07 13:42:08 -0600

    corrected names on forms for new users. added methods in user controller to make sure all forms are filled out and password is verified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,24 @@ class UsersController < ApplicationController
         erb :"/users/new"
     end
 
+    post '/user/new' do
+
+    end
+
     get '/user/login' do
         erb :"/users/login"
+    end
+
+    helpers do
+
+        def form_filled?
+            !params[:user][:email].empty? && !params[:user][:username].empty? && !params[:user][:password].empty? && !params[:password_verify].empty?
+        end
+
+        def password_verified?
+            params[:user][:password] == params[:password_verify]
+        end
+
     end
 
 end
